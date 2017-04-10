7372cf91cf6089b9ad5265bea8752be57ef66f12
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-04-10 22:11:12 +0200

    Working version against memory - no voting

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 const _ = require('koa-route')
-const render = require('./lib/render')
+views = require('koa-views')
 const Koa = require('koa')
 const app = new Koa()
 
 // Set up monk
 const monk = require('monk')
+const mongoUrl = process.env.MONGOHQ_URL || 'localhost:27017/koaVote_Prod';
+const db = monk(mongoUrl)
 const wrap = require('co-monk')
-// var mongoProdUri = process.env.MONGOHQ_URL || 'localhost:27017/koaVote_Prod';
-const db = monk('localhost/koaBlog')
-var posts = wrap(db.get('topics'))
+const topics = wrap(db.get('topics'))
 
-const routes = {
-  home: (ctx) => {
-    ctx.body = 'HELLO FROM ROUTES'
+const questionsList = [
+  {
+    "id": 1,
+    "title": "My first question",
+    "plusvotes": 40,
+    "minusvotes": 30,
+    "score": 0
   },
-  castVote: (ctx, topic) => {
-    ctx.body = 'CASTING VOTE' + topic
+  {
+    "id": 2,
+    "title": "My second question",
+    "plusvotes": 30,
+    "minusvotes": 40,
+    "score": 0
+  },
+  {
+    "id": 3,
+    "title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod animi nisi quisquam eius distinctio eveniet, quo iusto. Doloribus in, nisi, laboriosam voluptas atque dignissimos eum recusandae velit, quidem quisquam necessitatibus!",
+    "plusvotes": 30,
+    "minusvotes": 40,
+    "score": 0
   }
-}
+]
+
+// Set up views with koa-views
+app.use(views('./views', { map: { html: 'handlebars' } }))
+
 // route middleware
-app.use(_.get('/', routes.home))
-app.use(_.get('/:topic', routes.castVote))
+app.use(_.get('/', async (ctx) => {
+  let questions = await topics.find({})
+  await ctx.render('home.html', { questions: questions })
+}))
+app.use(_.get('/:id/up', async (ctx, id) => {
+  console.log('UP for ' + id)
+  ctx.redirect('/')
+}))
+app.use(_.get('/:id/down', async (ctx, id) => {
+  console.log('DOWN for ' + id)
+  ctx.redirect('/')
+}))
 
 // listen
 const port = process.env.PORT || 3000
